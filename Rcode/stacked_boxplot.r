d565f228b7589847f4ecf2bd7420b1b8859238db
tdelhomme <delhommet@students.iarc.fr>
2017-07-28 14:33:41 +0200

    use ggpubr package

--- Rcode/stacked_boxplot.r
+++ Rcode/stacked_boxplot.r
@@ -1,16 +1,22 @@
 # stacked boxplot with ggplot2
+library(ggpubr)
+
+
+myclasses = "classes"
+mysubclasses = "subclasses"
 
 df_transform <- function(df, classes, subclasses){
   colnames(df)[which(colnames(df)==classes)] = "cl"
   colnames(df)[which(colnames(df)==subclasses)] = "scl"
   colnames(df)[which(colnames(df) != "cl" & colnames(df) != "scl")][1] = "len"
   res = arrange(aggregate(len ~ cl + scl, df, length), cl, len)
-  ddply(res, "cl", transform, ylabel_pos = cumsum(len) - max(res$len)*0.03 )
+  ddply(res, "cl", transform, ylabel_pos = cumsum(len) )
 }
 
-ggplot(data = df_transform(ctDNA_dataset_disc_df[which(ctDNA_dataset_disc$SCLC_status==0),], myclasses, mysubclasses),
-       aes(x = cl, y = len, fill = scl )) +
-  geom_bar(stat="identity") +
-  geom_text(aes(y=ylabel_pos, label=len), color="white", size=3.5) +
-  scale_fill_brewer(palette="Paired") +
-  theme_minimal()
+
+df=df_transform(ctDNA_dataset_disc_df[which(ctDNA_dataset_disc$SCLC_status==1),], myclasses, mysubclasses)
+p = ggbarplot(df, x = "cl", y = "len",
+          fill = "scl", color = "scl",
+          label = TRUE, lab.col = "white", lab.pos = "in", palette = "NPG", legend.title=mysubclasses,
+          xlab=myclasses, ylab="counts")
+p + theme_minimal()

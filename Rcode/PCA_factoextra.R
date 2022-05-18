d6568715e35f06e2534822148c94997b4e16a514
Tiffany Delhomme <delhomme.tiffany@gmail.com>
2022-05-18 17:35:18 +0200

    Update PCA_factoextra.R

--- Rcode/PCA_factoextra.R
+++ Rcode/PCA_factoextra.R
@@ -1,4 +1,5 @@
 ### script to perform a PCA analysis using factoextra R package ###
+# inspiration from http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/
 
 library(factoextra)
 

5244dde4c740dd9a07549c60634961912bfafa08
Tiffany Delhomme <delhomme.tiffany@gmail.com>
2022-05-18 12:12:50 +0200

    Update PCA_factoextra.R

--- Rcode/PCA_factoextra.R
+++ Rcode/PCA_factoextra.R
@@ -1,1 +1,11 @@
 ### script to perform a PCA analysis using factoextra R package ###
+
+library(factoextra)
+
+res.pca <- prcomp(mydata, scale = TRUE)
+
+# eigen values plot
+fviz_eig(res.pca)
+
+# PCA plot
+fviz_pca_ind(res.pca)

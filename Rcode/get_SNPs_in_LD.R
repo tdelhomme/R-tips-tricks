8682cc74aabfd5d62f2338db2fd8fa643e55e5c8
Tiffany Delhomme <delhomme.tiffany@gmail.com>
2022-05-25 15:16:58 +0200

    Update get_SNPs_in_LD.R

--- Rcode/get_SNPs_in_LD.R
+++ Rcode/get_SNPs_in_LD.R
@@ -1,1 +1,10 @@
 # Script to returns the table of SNPs that are in linkage desequilibrium with input SNPs
+
+# core function:
+
+get_LD_snps <- function(snp, mytoken, r2=0.3, distance=50000){
+    df_proxies <- LDproxy(snp, pop="CEU", r2d="r2", token=mytoken)
+    df_proxies_filt = df_proxies[which(as.numeric(df_proxies$R2)>r2 & abs(as.numeric(df_proxies$Distance)) <= distance), ]
+    as.character(df_proxies$RS_Number)
+  }
+  

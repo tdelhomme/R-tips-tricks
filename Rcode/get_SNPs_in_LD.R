734ae830c9d861198d86eafcf243cb4d11bb9b19
Tiffany Delhomme <delhomme.tiffany@gmail.com>
2022-05-27 13:30:52 +0200

    Update get_SNPs_in_LD.R

--- Rcode/get_SNPs_in_LD.R
+++ Rcode/get_SNPs_in_LD.R
@@ -8,3 +8,16 @@ get_LD_snps <- function(snp, mytoken, r2=0.3, distance=50000){
     as.character(df_proxies$RS_Number)
   }
   
+# compute the LD SNPs:
+
+all_snps = as.character(read.table(input_file, h=T, stringsAsFactors = F)[,1]) #input_file should be a tab delimited file with first column=RS IDs
+  
+  for(snp in all_snps){
+    print(paste(date(), " working on snp: ", snp, sep=""))
+    restmp = try(get_LD_snps(snp))
+    if(!"try-error" %in% class(restmp)){
+      #restmp = get_LD_snps(snp)
+      res = restmp[which(grepl("rs", restmp$RS)),] 
+      if(match(snp, all_snps) == 1) { LD_snps = res } else { LD_snps = rbind(LD_snps, res) }
+    }
+  }

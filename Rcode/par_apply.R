6a1c690e181edaa18a12db1fadb411c26ebecdc5
Tiffany Delhomme <delhomme.tiffany@gmail.com>
2022-05-30 13:42:09 +0200

    Update par_apply.R

--- Rcode/par_apply.R
+++ Rcode/par_apply.R
@@ -1,1 +1,14 @@
 # how to use the doParallel R package to parallelize your computations
+
+# load the package
+library(doParallel)
+
+# create the cluster
+clus <- makeCluster(nb_cpu)
+clusterExport(clus, list("object1", "object2")) # put here the functions/variables that you will need in the parlapply
+
+# run the loop
+par_res = parallel::parLapply(clus, my_input_vector, function(s){
+      res = s # here put all the stuff you want to apply to the s item
+      return(res)
+})

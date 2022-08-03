168ffda1b76f7fd288d7804e8abdae62a2f8bd80
Tiffany Delhomme <delhomme.tiffany@gmail.com>
2022-08-03 14:20:31 +0200

    add how to get gene coordinate in the genome

--- Rcode/manipulate_genomes.r
+++ Rcode/manipulate_genomes.r
@@ -11,3 +11,10 @@ library(GenomicRanges)
 findOverlaps(makeGRangesFromDataFrame( read.table(bed1, h=T, stringsAsFactors=F) ), 
              makeGRangesFromDataFrame( read.table(bed2, h=T, stringsAsFactors=F) ) )
 # this will return a Hits object with 2 columns: queryHits subjectHits (ids of bed1 in bed2 and bed2 in bed1)
+
+# 3. Get genomic coordinates of a particular gene
+library(Homo.sapiens)
+gene_list = genes(TxDb.Hsapiens.UCSC.hg19.knownGene) # for hg19
+tt = as.data.frame(org.Hs.egSYMBOL) # for each gene ID contained in gene_list, returns the corresponding gene name
+id = as.numeric(tt[which(tt$symbol == GENE_NAME), "gene_id"])))
+gene_list[which(gene_list$gene_id == id),]
